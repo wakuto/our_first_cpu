4ef633441ea0c1970467bb0f8cb509c24d7cc8cb
sushiyoshi <eito.masuyama@icloud.com>
2023-11-04 15:55:56 +0900

    fix alu src_a bug

--- pipeline/cpu.sv
+++ pipeline/cpu.sv
@@ -293,7 +293,7 @@ module CPU(
 
     ALU alu(
         .alu_control(alu_control_e),
-        .srca(rd1_e),
+        .srca(pc_alu_src_a_e),
         .srcb(srcb_e),
         .alu_result(alu_result_e),
         .zero(zero_e)
